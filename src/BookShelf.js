e5654aa109793a02349f0e0082f87b952bc80411
Muniba206 <talha.muniba@gmail.com>
2018-08-08 11:07:50 +0200

    Added interface to show the book shelf with books

---
+++ src/BookShelf.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Book from './Book';
+import PropTypes from  'prop-types';
+import sortBy from 'sort-by';
+import WaitingScreen from './WaitingScreen';
+
+/** BookShelf renders BookShelfs and their categories. Imports books and renders components **/
+
+class BookShelf extends Component {
+	static propTypes = {
+		title: PropTypes.string.isRequired,
+		books: PropTypes.array.isRequired ,
+		onChangeCategory: PropTypes.func.isRequired
+	}
+	
+	state= {
+		loading: false
+	}
+	
+	changeCategory = (id, category) => {
+		this.setState({ loading: true });
+		this.props.onChangeCategory(id, category);
+	}
+	
+	componentWillReceiveProps() {
+		this.setState ({ loading: false });
+	}
+	
+	render () {
+		const { title, books } = this.props;
+		const { loading } = this.state;
+		
+		if (loading) return <WaitingScreen text='Updating bookshelves...' />;
+
+    let bookshelves = books
+      .sort(sortBy('title'))
+      .map(book => (
+        <li key={book.id}>
+          <Book {...book} onChangeCategory={this.changeCategory} />
+        </li>
+      ));
+
+    let bookshelfCount = bookshelves.length;
+
+    return (
+      <div className='bookshelf'>
+        <h2 className='bookshelf-title'>
+          {title} <span className='bookshelf-count'>({bookshelfCount})</span>
+        </h2>
+        {bookshelfCount === 0 && (
+          <p>There is no book in this bookshelf.</p>
+        )}
+        <div className='bookshelf-books'>
+          {bookshelfCount > 0 && (
+            <ol className='books-grid'>
+              {bookshelves}
+            </ol>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BookShelf;
---
+++ src/ListBooks.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BookShelf from './BookShelf';
+import { Link } from 'react-router-dom';
+
+/** Main interface to showcase Books and BookShelfs **/
+
+class ListBooks extends Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    onChangeCategory: PropTypes.func.isRequired
+  }
+
+  render() {
+    const { books, onChangeCategory } = this.props;
+
+    return(
+      <div className='list-books'>
+        <div className='list-books-title'>
+          <h1>MyReads</h1>
+        </div>
+        <div className='list-books-content'>
+          <div>
+            <BookShelf
+              title='Currently Reading'
+              books={books.filter(book => book.shelf === 'currentlyReading')}
+              onChangeCategory={onChangeCategory}
+            />
+            <BookShelf
+              title='Want to Read'
+              books={books.filter(book => book.shelf === 'wantToRead')}
+              onChangeCategory={onChangeCategory}
+            />
+            <BookShelf
+              title='Read'
+              books={books.filter(book => book.shelf === 'read')}
+              onChangeCategory={onChangeCategory}
+            />
+          </div>
+        </div>
+        <div className='open-search'>
+          <Link to='/search'>Add a book</Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBooks;
